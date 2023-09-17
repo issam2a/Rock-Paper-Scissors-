8fee5ca0821f13ed8f0944a57ab93affdb9f431b
issam <issamissa2a@gmail.com>
2023-09-18 00:04:23 +0300

    creating a for loop that run the program for 5 rounds or if the player score is <=3 or the computer score <=3

---
+++ RPS.js
@@ -0,0 +1,13 @@
+let playerScore = 0;
+let computerScore = 0;
+
+for (let i = 0; i < 10 && playerScore <= 3 && computerScore <= 3; i++) {
+  const player = getPlayerSelection();
+  const computer = getComputerSelection();
+  function getComputerSelection() {
+    const computerSelection = ["Rock", "paper", "Scissors"];
+    return computerSelection[
+      Math.floor(Math.random() * computerSelection.length)
+    ].toLowerCase();
+  }
+}
