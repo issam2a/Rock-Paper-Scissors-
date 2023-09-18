52e12534b2a1a6a0cedbdc5a567ec9592585c0c1
issam <issamissa2a@gmail.com>
2023-09-18 22:00:05 +0300

    createing an if else statment to show the final score

--- RPS.js
+++ RPS.js
@@ -14,4 +14,71 @@ for (let i = 0; i < 10 && playerScore <= 3 && computerScore <= 3; i++) {
     let playerSelection = prompt("choose your weapon!!");
     return playerSelection.toLowerCase();
   }
+  function PlayRound(computerSelection, playerSelection) {
+    if (
+      playerSelection === "rock" ||
+      playerSelection === "paper" ||
+      playerSelection === "scissors"
+    ) {
+      if (computerSelection === playerSelection) {
+        console.log(`tie ... 
+                computer :${computerSelection}, computer score :${computerScore}
+                player selection : ${playerSelection} , player score : ${playerScore}`);
+      } else if (computerSelection === "paper" && playerSelection === "rock") {
+        computerScore++;
+        console.log(`computer :${computerSelection} computer score: ${computerScore}
+            player : ${playerSelection} player score : ${playerScore}
+            You loss ! ${computerSelection} beats ${playerSelection} `);
+      } else if (
+        computerSelection === "rock" &&
+        playerSelection === "scissors"
+      ) {
+        computerScore++;
+        console.log(`computer :${computerSelection} computer score: ${computerScore}
+            player : ${playerSelection} player score : ${playerScore}
+            You loss ! ${computerSelection} beats ${playerSelection} `);
+      } else if (
+        computerSelection === "scissors" &&
+        playerSelection === "paper"
+      ) {
+        computerScore++;
+        console.log(`computer :${computerSelection} computer score: ${computerScore}
+            player : ${playerSelection} player score : ${playerScore}
+            You loss ! ${computerSelection} beats ${playerSelection} `);
+      } else if (
+        computerSelection === "paper" &&
+        playerSelection === "scissors"
+      ) {
+        computerScore++;
+        console.log(`computer :${computerSelection} computer score: ${computerScore}
+            player : ${playerSelection} player score : ${playerScore}
+            You win ! ${playerSelection} beats ${computerSelection} `);
+      } else if (computerSelection === "rock" && playerSelection === "paper") {
+        computerScore++;
+        console.log(`computer :${computerSelection} computer score: ${computerScore}
+            player : ${playerSelection} player score : ${playerScore}
+            You win ! ${playerSelection} beats ${computerSelection} `);
+      } else if (
+        computerSelection === "scissors" &&
+        playerSelection === "rock"
+      ) {
+        computerScore++;
+        console.log(`computer :${computerSelection} computer score: ${computerScore}
+            player : ${playerSelection} player score : ${playerScore}
+            You win ! ${playerSelection} beats ${computerSelection} `);
+      }
+    } else {
+      alert("Enter the right weapon !!");
+    }
+    PlayRound(computerSelection, playerSelection);
+  }
+}
+if (playerScore > computerScore) {
+  console.log(
+    `you are victoris , your score is : ${playerScore} computer score is :${computerScore}`
+  );
+} else {
+  console.log(
+    ` you lost the war with machines your score is : ${playerScore} computer score is :${computerScore}`
+  );
 }
